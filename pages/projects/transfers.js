bc36bcc0c28db1a6fa87970930c44ca1f8d8167b
SteveTheSloth <lucas.voigt@web.de>
2023-10-20 16:26:43 +0100

    Updated text elements for personal projects.

--- pages/projects/transfers.js
+++ pages/projects/transfers.js
@@ -19,16 +19,16 @@ export default function Transfers() {
       <MainParagraph
         heading="Football Transfer Analysis & Webscraping"
         link="https://github.com/SteveTheSloth/Football_Transfer_Analysis"
-        text="While I had not yet completely figured out which carreer path to pursue,
-            data analysis had been a serious contender for a while. While I did not end up pursuing this path further in the end, I was still able to learn a lot from this
+        text="While I had not yet completely set my mind on which carreer path to pursue,
+            data analysis had been a serious contender. Even though I did not end up pursuing this path further, I was able to learn a lot from this
             project. I used new tools and python libraries for the first time, analysed the structure of websites to ensure that my web scraping produced usable data, and tested my
-            abilities to work with a medium sized dataset. Statistics were an important part of my University degree and I do enjoy working with data and
-            figuring out correlations to support or disprove a theoretical argument. Coincidentally I am also a big football fan and have supported my club Borussia Dortmund through
-            highs and lows over the past 25 years. Following the German Bundesliga for such a substantial period of time, I couldn't help but wonder about some more
-            recent developments that seemed significant to me. Over the last years, I felt that apart from Bayern Munich, pretty much all of the Bundesliga clubs were 
+            abilities to work with a medium sized dataset. Statistics were an important part of my University degree and I enjoy working with data and
+            figuring out correlations to support or disprove a theoretical argument. Coincidentally I am also a big football fan and have supported my team through
+            highs and lows over the past 25 years. Following the German Bundesliga for such a substantial period of time, I started to wonder about some more
+            recent developments that seemed significant to me. Over the last years, I felt that apart from Bayern Munich, all of the Bundesliga clubs were 
             on the decline. Although absolute turnover numbers are continuously increasing (interrupted of course by the Covid-19 pandemic), comparatively, the Bundesliga
-            seemed to lose track of the big leagues. This was supported by international results of German clubs where only few successes stand against a plethora of examples
-            of rather shameful defeats. What interested me was to figure out if this decline was somehow reflected in the transfers to and from Bundesliga clubs and if there
+            seems to lose track of the European top leagues. This observation was supported by rather subpar international results of German clubs. 
+            What intrigued me was to figure out if this decline was somehow reflected in the transfers to and from Bundesliga clubs and if there
             is a trend that could be visualised by analysing them."
       />
       <Image
@@ -40,11 +40,10 @@ export default function Transfers() {
         alt="Bundesliga Transfers Intra-Bundesliga Graph"
       />
       <SubParagraph
-        heading="Getting the data - Web Scraping with scrapy"
-        text="As with any data analysis, my first step was to somehow acquire the data to work with. As I was a football fan, I
-            knew the go-to websites when it comes to transfers and used the web scraping library scrapy to store the transfer data in a JSON file. I had to overcome some challenges like incoherent formatting 
-            over the years, duplicate transfers for transfers between two Bundesliga clubs, and partially incomplete data, but managed to configure the web scraping algorithm to produce a fairly coherent dataset.
-            Of course I had to still clean the dataset before working with it."
+        heading="Acquiring the data - Web Scraping with scrapy"
+        text="Being a football fan, I am familiar with the most popular websites covering transfers. I used the web scraping library scrapy collect the data from a website and stored 
+            it in a JSON file. This involved overcoming challenges like incoherent formatting, duplicate data entries for transfers between two Bundesliga clubs, and partially incomplete data. 
+            I was able to configure the web scraping algorithm to produce a fairly coherent dataset, reducing the need to clean up the data significantly."
       />
       <Image
         priority
@@ -56,11 +55,11 @@ export default function Transfers() {
       />
       <SubParagraph
         heading="Analysing the data - jupyter, pandas, and matplotlib"
-        text="I formulated several hypotheses to be tested by my analysis before I started to work with the data. These 
+        text="I formulated several hypotheses to be tested before I started to work with the data. These 
             included for example: 'The average player age for transfers declined over time' or 'The number of transfers from the top leagues declined over time'. I did not stick to these hypotheses
-            very strictly, as I first wanted to explore the data and learn how to properly work with the libraries and tools I was using, rather than producing academically correct results. 
-            I then set up a jupyter notebook and started to work with the data using the python libraries pandas and matplotlib for data manipulation and visualisation. While my analysis is nowhere near complete, I was able to
-            figure out several trends in the data and visualise them."
+            very strictly, as I intended to explore the data and learn how to properly work with the libraries and tools I was using, rather than producing academically correct results. 
+            I set up a jupyter notebook and started to work with the data using the python libraries pandas and matplotlib for data manipulation and visualisation. I was able to
+            figure out several trends in the data and visualise them, while at the same time expanding my knowledge on the tools and libraries I worked with."
       />
       <Image
         priority
